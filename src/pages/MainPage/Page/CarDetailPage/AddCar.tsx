18da828abc76eb21b9f7169da5fc6edf04f5e0f2
kritsada-a60 <kritsada.a60@rsu.ac.th>
2022-12-29 17:48:48 +0700

    addcar page

--- src/pages/MainPage/Page/CarDetailPage/AddCar.tsx
+++ src/pages/MainPage/Page/CarDetailPage/AddCar.tsx
@@ -62,6 +62,32 @@ ctm_mqtt_code: string;
 // u_id: number;
 };
 
+type MyDorpDownData = {
+    ctm_address: string;
+    ctm_amphur: string;
+    ctm_bank: string;
+    ctm_bank_no: string;
+    ctm_cno: string;
+    ctm_contact_name: string;
+    ctm_id: string;
+    ctm_mail: string;
+    ctm_mobile: string;
+    ctm_mqtt_code: any;
+    ctm_name: string;
+    ctm_province: string;
+    ctm_tel: string;
+    ctm_tumbon: string;
+    ctm_zipcode: string;
+    ctmt_id: number;
+    ctmt_name: string;
+};
+
+type MyDorpDownData2 = {
+    pt_description: any,
+    pt_id: number,
+    pt_name: string;
+};
+
 
 
 export interface ISAddCarPageProps {}
@@ -76,9 +102,19 @@ const baseURLUpdateEdit ="http://54.86.117.200:5000/car/edit"
 
 const baseURLUpdateAdd ="http://54.86.117.200:5000/car/add"
 
+const baseURLDorpDown ="http://54.86.117.200:5000/customer/list"
+
+const baseURLDorpDown2 ="http://54.86.117.200:5000/powertype/list"
+
 const AddCarPage: React.FunctionComponent<ISAddCarPageProps> = (props) => {
 
     const [post, setpost] = useState<MyDataPost[]>([]);
+
+    const [DorpDownData, setDorpDownData] = useState<MyDorpDownData[]>([]);
+
+    const [DorpDownData2, setDorpDownData2] = useState<MyDorpDownData2[]>([]);
+
+    const [DorpDownData3, setDorpDownData3] = useState<MyDorpDownData2[]>([]);
 
     
     const [Input1, setInput1] = useState("") 
@@ -86,7 +122,31 @@ const AddCarPage: React.FunctionComponent<ISAddCarPageProps> = (props) => {
     const [Input3, setInput3] = useState("") 
     const [Input4, setInput4] = useState("")
     const [Input5, setInput5] = useState("") 
-    const [Input6, setInput6] = useState("") 
+    const [Input6, setInput6] = useState("")
+    const [Input7, setInput7] = useState("") 
+    const [Input8, setInput8] = useState("")
+
+    useEffect(()=>{
+      axios.get(baseURLDorpDown).then((response) => {
+        setDorpDownData(response.data.data)
+        
+        // const result = FirstData.filter((member) => {
+        //   return member.ctmt_id = 2
+        // })
+      })
+      axios.post(baseURLDorpDown2,{
+        pc_id: 1
+      }).then((response) => {
+        setDorpDownData2(response.data.data)
+        console.log(response.data)
+      })
+      axios.post(baseURLDorpDown2,{
+        pc_id: 2
+      }).then((response) => {
+        setDorpDownData3(response.data.data)
+        console.log(response.data)
+      })
+    },[])
 
     const handleSubmit = (e:any) => {
     e.preventDefault();
@@ -100,12 +160,14 @@ const AddCarPage: React.FunctionComponent<ISAddCarPageProps> = (props) => {
         // ctm_bank_no: Input4,
         // ctm_contact_name: Input5,
         // ctm_address : Input6,
-        ctm_id: Number(Input1),
-        bt_pt_id: Number(Input2),
-        cgt_pt_id: Number(Input3),
-        c_license_plate: Input4,
-        c_mqtt_code: Input5,
-        u_id: Number(Input6),
+        c_id: 1,
+        ctm_id: Number(Input2),
+        bt_pt_id: Number(Input3),
+        cgt_pt_id: Number(Input4),
+        c_license_plate: Input1,
+        c_mqtt_code: "TT_3",
+        c_active: "1",
+        u_id: 1,
       })
       .then((res) => {
         console.log(res.data);
@@ -135,6 +197,10 @@ const AddCarPage: React.FunctionComponent<ISAddCarPageProps> = (props) => {
         console.log(Input1,"input")
     }, [Input1]);
 
+    const resultDorpDownData = DorpDownData.filter((member) => {
+      return member.ctmt_id == 2
+    })
+
 
 
 
@@ -144,54 +210,141 @@ const AddCarPage: React.FunctionComponent<ISAddCarPageProps> = (props) => {
           <p style={{margin:'5vh 30vw',justifyContent:'center' ,fontSize:'36px'}}>เพิ่มข้อมูลรถ</p>
           <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
             <form >
-              {/* <div style={{margin:'2.5vh 0'}}>
+              <div style={{margin:'2.5vh 0',display:'flex',justifyContent:'center',alignItems:'center'}}>
                 <label>
-                  <TextField type="ut_name" name="ut_name" style={{margin:'0 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="ชื่อลูกค้า"
+                  <p style={{margin:'0vh 5vw',borderColor:'black', width:'15vw',fontSize:'18px',fontWeight:'bold'}}>ทะเบียนรถ</p>
+                  <TextField type="ut_name" name="ut_name" style={{margin:'1vh 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="ชื่อลูกค้า"
                   // value={Bnumber}
                   value={Input1}
                   onChange={(e) => {setInput1(e.target.value)}}
                   />       
                 </label>
                 <label>
-                  <TextField type="ut_name" name="ut_name" style={{margin:'0 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="เลขทะเบียนการค้า"
-                  // value={Bnumber}
-                  value={Input2}
-                  onChange={(e) => {setInput2(e.target.value)}}
-                  />       
-                </label>
-              </div> */}
-              <div style={{margin:'2.5vh 0'}}>
-                {/* <label>
-                  <TextField type="ut_name" name="ut_name" style={{margin:'0 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="ชื่อธนาคาร"
-                  // value={Bnumber}
-                  value={Input3}
-                  onChange={(e) => {setInput3(e.target.value)}}
-                  />       
-                </label> */}
-                <label>
-                  <TextField type="ut_name" name="ut_name" style={{margin:'0 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="ทะเบียนรถ"
-                  // value={Bnumber}
-                  value={Input4}
-                  onChange={(e) => {setInput4(e.target.value)}}
-                  />       
+                    <p style={{margin:'0vh 5vw',borderColor:'black', width:'15vw',fontSize:'18px',fontWeight:'bold'}}>อู่</p>
+                    <Select type="ประเภทกิจการ" name="" style={{margin:'1vh 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="ประเภทกิจการ"
+                    value={Input2}
+                    onChange={(e) => {setInput2(e.target.value)}}
+                    // label="Age"
+                    displayEmpty
+                    inputProps={{ 'aria-label': 'Without label' }}
+                    >
+                      
+                    <MenuItem value="">
+                        <em></em>
+                    </MenuItem>
+                    {resultDorpDownData?.length &&
+                        resultDorpDownData.map((e: any, i: number) => {
+                        return (
+                            <MenuItem key={i} value={e.ctm_id}
+                            // onChange={e =>{
+                            //     console.log(e,"E")
+                            //     setNumberDorpDown(e.sv_id)
+                            // }}
+                            >
+                            {e.ctm_name}
+                            </MenuItem>
+                        );
+                    })}
+                    </Select>
+                    {/* {NumberDorpDown}          */}
                 </label>
               </div>
-              {/* <div style={{margin:'2.5vh 0'}}>
+              <div style={{margin:'2.5vh 0',display:'flex',justifyContent:'center',alignItems:'center'}}>
                 <label>
-                  <TextField type="ut_name" name="ut_name" style={{margin:'0 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="ชื่อผู้ติดต่อ"
+                    <p style={{margin:'0vh 5vw',borderColor:'black', width:'15vw',fontSize:'18px',fontWeight:'bold'}}>หัวชาร์จ</p>
+                    <Select type="หัวชาร์จ" name="" style={{margin:'1vh 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="หัวชาร์จ"
+                    value={Input3}
+                    onChange={(e) => {setInput3(e.target.value)}}
+                    // label="Age"
+                    displayEmpty
+                    inputProps={{ 'aria-label': 'Without label' }}
+                    >
+                      
+                    <MenuItem value="">
+                        <em></em>
+                    </MenuItem>
+                    {DorpDownData2?.length &&
+                        DorpDownData2.map((e: any, i: number) => {
+                        return (
+                            <MenuItem key={i} value={e.pt_id}
+                            // onChange={e =>{
+                            //     console.log(e,"E")
+                            //     setNumberDorpDown(e.sv_id)
+                            // }}
+                            >
+                            {e.pt_name}
+                            </MenuItem>
+                        );
+                    })}
+                    </Select>
+                    {/* {NumberDorpDown}          */}
+                </label>
+                <label>
+                    <p style={{margin:'0vh 5vw',borderColor:'black', width:'15vw',fontSize:'18px',fontWeight:'bold'}}>แบตเตอร์รี่</p>
+                    <Select type="แบตเตอร์รี่" name="" style={{margin:'1vh 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="แบตเตอร์รี่"
+                    value={Input4}
+                    onChange={(e) => {setInput4(e.target.value)}}
+                    // label="Age"
+                    displayEmpty
+                    inputProps={{ 'aria-label': 'Without label' }}
+                    >
+                      
+                    <MenuItem value="">
+                        <em></em>
+                    </MenuItem>
+                    {DorpDownData3?.length &&
+                        DorpDownData3.map((e: any, i: number) => {
+                        return (
+                            <MenuItem key={i} value={e.pt_id}
+                            // onChange={e =>{
+                            //     console.log(e,"E")
+                            //     setNumberDorpDown(e.sv_id)
+                            // }}
+                            >
+                            {e.pt_name}
+                            </MenuItem>
+                        );
+                    })}
+                    </Select>
+                    {/* {NumberDorpDown}          */}
+                </label>
+              </div>
+              <div style={{margin:'2.5vh 0',display:'flex',justifyContent:'center',alignItems:'center'}}>
+                <label>
+                  <p style={{margin:'0vh 5vw',borderColor:'black', width:'15vw',fontSize:'18px',fontWeight:'bold'}}>ไมล์เริ่มต้น</p>
+                  <TextField type="ut_name" name="ut_name" style={{margin:'1vh 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="ชื่อผู้ติดต่อ"
                   // value={Bnumber}
                   value={Input5}
                   onChange={(e) => {setInput5(e.target.value)}}
                   />       
                 </label>
                 <label>
-                  <TextField type="ut_name" name="ut_name" style={{margin:'0 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="ที่อยู่"
+                  <p style={{margin:'0vh 5vw',borderColor:'black', width:'15vw',fontSize:'18px',fontWeight:'bold'}}>ความเร็วรถ</p>
+                  <TextField type="ut_name" name="ut_name" style={{margin:'1vh 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="ที่อยู่"
                   // value={Bnumber}
                   value={Input6}
                   onChange={(e) => {setInput6(e.target.value)}}
                   />       
                 </label>
-              </div> */}
+              </div>
+              <div style={{margin:'2.5vh 0',display:'flex',justifyContent:'center',alignItems:'center'}}>
+                <label>
+                  <p style={{margin:'0vh 5vw',borderColor:'black', width:'15vw',fontSize:'18px',fontWeight:'bold'}}>เลขไมล์</p>
+                  <TextField type="ut_name" name="ut_name" style={{margin:'1vh 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="ชื่อผู้ติดต่อ"
+                  // value={Bnumber}
+                  value={Input7}
+                  onChange={(e) => {setInput5(e.target.value)}}
+                  />       
+                </label>
+                <label>
+                  <p style={{margin:'0vh 5vw',borderColor:'black', width:'15vw',fontSize:'18px',fontWeight:'bold'}}>วันที่เริ่มใช้งาน</p>
+                  <TextField type="ut_name" name="ut_name" style={{margin:'1vh 5vw',backgroundColor:'white',borderColor:'black', width:'15vw'}} placeholder="ที่อยู่"
+                  // value={Bnumber}
+                  value={Input8}
+                  onChange={(e) => {setInput6(e.target.value)}}
+                  />       
+                </label>
+              </div>
 
               <div style={{display:'flex',justifyContent:'center',alignItems:'center'}}>
                 <Button style={{color:'white', backgroundColor:'#6CDCC0',margin:'2.5vh 2.5vw'}}
