4520855b1c85b184a6e39535f917bfafeba22c8d
kritsada-a60 <kritsada.a60@rsu.ac.th>
2023-01-05 15:47:35 +0700

    fix update

--- src/pages/MainPage/Page/MapPage/GoogleMap.tsx
+++ src/pages/MainPage/Page/MapPage/GoogleMap.tsx
@@ -1,6 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 import { GoogleMap, InfoWindow, Marker } from '@react-google-maps/api';
 import IconCar from '../../../../img/Icon/IconCar.png';
+import axios from 'axios';
+
+type MarkersData = {
+    bt_pt_id: number;
+    bt_pt_name: string;
+    c_active: string;
+    c_capacity: number;
+    c_gps_signal: any;
+    c_gps_status: any;
+    c_id: number;
+    c_lat: any;
+    c_license_plate: string;
+    c_lng: any;
+    c_mileage: any;
+    c_mileage_init: any;
+    c_mqtt_code: string;
+    c_place: any;
+    c_speed: any;
+    c_status: string;
+    cgt_pt_id: number;
+    cgt_pt_name: string;
+    cs_id: any;
+    ctm_id: number;
+    ctm_name: string;
+};
+
+type MyMarkersData = {
+    c_id: number;
+    c_lat: any;
+    c_license_plate: string;
+    c_lng: any;
+};
 
 const markers = [
     {
@@ -29,7 +61,62 @@ const markers = [
     }
 ];
 
+
+
+
+const URLMerkersData = "http://54.86.117.200:5000/car/list"
+
 function Map() {
+
+    const [MarkersData, setMarkersData] = useState<MyMarkersData[]>([]);
+
+    const [MarkersId, setMarkersId] = useState<MarkersData[]>([]);
+    const [MarkersName, setMarkersName] = useState<MarkersData[]>([]);
+    const [MarkersLat, setMarkersLat] = useState<MarkersData[]>([]);
+    const [MarkersLng, setMarkersLng] = useState<MarkersData[]>([]);
+
+    const [data, setdata] = useState("");
+
+    useEffect(() => {
+        axios
+        .post(URLMerkersData,{
+            ctm_id : "2"
+        })
+        .then((res) => {
+            console.log(res.data.data);
+            setMarkersData(res.data.data)
+            // setMarkersId(res.data.data[0].c_id);
+            // setdata(res.data.data.c_id);
+            // setMarkersName(res.data.data.c_license_plate);
+            // setMarkersLat(res.data.data.c_lat);
+            // setMarkersLng(res.data.data.c_lng);
+            // console.log(MarkersId)
+            // Object.entries(res.data.data).forEach(([key,value]) =>{
+            //     console.log(`${key} : ${value}`)
+            // })
+        })
+        .catch((err) => console.error(err));
+
+    }, []);
+
+    function testmerker (){
+        var data = [];
+        for(var i in MarkersData) {
+            data.push(MarkersData[i].c_id);
+        }
+
+        console.log(data);
+
+    }
+
+    // useEffect(() =>{
+    //   console.log(MarkersId,"This MarkersId")
+    // }, [MarkersId]);
+
+    // useEffect(() =>{
+    //   console.log(data,"This data")
+    // }, [data]);
+
     const [activeMarker, setActiveMarker] = useState(null);
 
     const handleActiveMarker = (marker: any) => {
@@ -37,6 +124,7 @@ function Map() {
             return;
         }
         setActiveMarker(marker);
+        testmerker();
     };
 
     const handleOnLoad = (map: any) => {
